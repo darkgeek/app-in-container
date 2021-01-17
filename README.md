5dbc04fcf9746112d0fa53b11962489fb3bcf0fc
Justin Yang <linuxjustin@gmail.com>
2021-01-17 08:37:20 +0000

    Make it more robust

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # app-in-container
 Run you app in container
+
+# Usage
+Start server:
+
+`./run-app-in-container-server.fish 9999`
+
+Then, just send request to start app:
+
+`curl http://127.0.0.1:9999/qutebrowser/DISPLAY=:1`
--- run-app-in-container-server.fish
+++ run-app-in-container-server.fish
@@ -7,6 +7,16 @@ function find_handler_path
     echo (string join / $parts)
 end
 
+function pre_check
+    set -l arg_lenth (count $argv)
+    if test $arg_lenth -eq 0
+        echo "Usage: run-app-in-container-server.fish 127.0.0.1 9999"
+        exit 1
+    end
+end
+
+pre_check $argv
+
 set FIFO_PATH /tmp/run-container-app.fifo
 set HANDLER_NAME handler.fish
 set HANDLER_PATH (find_handler_path)
@@ -17,5 +27,5 @@ end
 
 # Start server in the infinite loop
 while true
-    cat $FIFO_PATH | xargs -n 1 -d '\n' $HANDLER_PATH 2>&1 | nc -lv 9999 > $FIFO_PATH
+    cat $FIFO_PATH | xargs -n 1 -d '\n' $HANDLER_PATH 2>&1 | nc -lv $argv > $FIFO_PATH
 end
